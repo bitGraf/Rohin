754c619514ffd6752b363ca734f1142af04ed0c2
Nick-Trot <49577190+Nick-Trot@users.noreply.github.com>
2020-01-07 16:53:09 -0500

    Added new function for A* plan reconstruction

--- src/Engine/GameObject/Astar.cpp
+++ src/Engine/GameObject/Astar.cpp
@@ -12,7 +12,7 @@ static int numOpened = 0;
 static int numClosed = 0;
 
 // Heuristic estimator, remaining distance = number of mismatched relevant atoms
-static int calc_h(worldstate_t fr, worldstate_t to)
+static int hCalc(worldstate_t fr, worldstate_t to)
 {
 	const u64 care = (to.relevance ^ -1LL);
 	const u64 diff = ((fr.values & care) ^ (to.values & care));
@@ -23,7 +23,7 @@ static int calc_h(worldstate_t fr, worldstate_t to)
 }
 
 // Looks up worldstate in our opened set
-static int idx_in_opened(worldstate_t ws)
+static int idxOpened(worldstate_t ws)
 {
 	for (int i = 0; i < numOpened; ++i)
 		if (opened[i].ws.values == ws.values) return i;
@@ -31,9 +31,41 @@ static int idx_in_opened(worldstate_t ws)
 }
 
 // Looks up worldstate in our closed set
-static int idx_in_closed(worldstate_t ws)
+static int idxClosed(worldstate_t ws)
 {
 	for (int i = 0; i < numClosed; ++i)
 		if (closed[i].ws.values == ws.values) return i;
 	return -1;
+}
+
+// Reconstruct path from last to initial node
+static void planReconstruction(ActionPlanner* ap, astarnode_t* goalNode, const char** plan, worldstate_t worldstates, int* szPlan)
+{
+	astarnode_t* curNode = goalNode;
+	int idx = *szPlan - 1;
+	int numSteps = 0;
+	while (curNode && curNode->actionName)
+	{
+		if (idx >= 0 )
+		{
+			plan[idx] = curNode->actionName;
+			worldstates[idx] = curNode->ws;
+			const int i = idxClosed(curNode->parentWs);
+			curNode = (i == -1) ? 0 : closed + i;
+		}
+		--idx;
+		numSteps++;
+	}
+	idx++; // We point to last filled here
+	
+	if ( idx > 0 )
+		for ( int i=0; i<numSteps; ++i )
+		{
+			plan[i] = plan[ i + idx];
+			worldstates[i] = worldstates[ i + idx ];
+		}
+	if ( idx < 0 )
+		LOGE( "Plan of size %d cannot be returned in buffer of size %d", numsteps, *szPlan );
+	
+	*szPlan = numSteps;
 }
