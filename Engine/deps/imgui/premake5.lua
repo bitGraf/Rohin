72af755e20951ee5f86e288d45decc12d42bb703
bitgraf <ablaskov27@gmail.com>
2022-11-13 18:15:22 -0600

    fix linking/path issues with new submodules. Game runs!

--- Engine/deps/imgui/premake5.lua
+++ Engine/deps/imgui/premake5.lua
@@ -3,21 +3,36 @@ project "ImGui"
 	language "C++"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
 		"imgui/imconfig.h",
 		"imgui/imgui.h",
 		"imgui/imgui.cpp",
+		"imgui/imgui_demo.cpp",
 		"imgui/imgui_draw.cpp",
-		"imgui/imgui_internal.h",
 		"imgui/imgui_widgets.cpp",
+		"imgui/imgui_tables.cpp",
+		"imgui/imgui_internal.h",
 		"imgui/imstb_rectpack.h",
 		"imgui/imstb_textedit.h",
 		"imgui/imstb_truetype.h",
-		"imgui/imgui_demo.cpp"
+		"imgui/backends/imgui_impl_opengl3.cpp",
+		"imgui/backends/imgui_impl_glfw.cpp"
 	}
+
+	includedirs
+    {
+        "imgui",
+		"%{wks.location}/Engine/deps/GLFW/GLFW/include"
+    }
+
+	links
+    {
+        "GLFW",
+        "GLAD",
+    }
 
 	filter "system:windows"
 		systemversion "latest"
